2c69ecf23aa3bb9d47f8cba7831ef4c49967d8cf
johnpablosilva <johnpablooliveira85@gmail.com>
2024-09-30 15:05:27 -0500

    atividade 01

--- Atividade 01/src/App.java
+++ Atividade 01/src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int n1 = 2;
+        int n2 = 3;
+        System.out.println(n1+n2);
+        System.out.println(n1*n2);
+        double n3 = (double) n1/n2;
+        System.out.println(n3);
+        System.out.println(n1*n2);
+        double n4 = (double) n1 % n2;
+        System.out.println(n4);
+        float n5 = 0.5f;
+        float n6 = 0.75f;
+        System.out.println(n5+"+"+n6+"="+(n5+n6));
+        System.out.println(n5+"+"+n6+"="+(n5-n6));
+        float n7 = 10.0f;
+        float n8 = 2.0f;
+        System.err.println(n7 / n8);
+        System.err.println(n7 * n8);
     }
 }
